1105cf39e52d481dd5d06da89604d21882691ce0
Abhishek Shankar <abhishekshankar27082002@gmail.com>
2024-09-03 11:42:29 +0530

    Time: 35 ms (77.51%) | Memory: 16.6 MB (37.00%) - LeetSync

---
+++ Easy/2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        s = ''.join(str(ord(ch)-96) for ch in s)
+        for _ in range(k):
+            s = sum(map(int, str(s)))
+        return s
